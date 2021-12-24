9c57fe5b30ff0a426389e9affb3798b927ac1282
ViniciusHP <viniciushp97@gmail.com>
2021-12-24 15:34:12 -0300

    Aula Parâmetros compostos

---
+++ alura/04-postgresql-desenvolva-com-pl-pgsql/04-parametros-compostos.sql
@@ -0,0 +1,15 @@
+CREATE TABLE instrutor(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	salario DECIMAL(10,2)
+);
+
+INSERT INTO instrutor (nome, salario) VALUES ('Vinicius Dias', 100);
+
+SELECT * FROM instrutor;
+
+CREATE FUNCTION dobro_do_salario(instrutor) RETURNS DECIMAL AS $$
+	SELECT $1.salario * 2 AS dobro;
+$$ LANGUAGE SQL;
+
+SELECT nome, dobro_do_salario(instrutor.*) FROM instrutor;
