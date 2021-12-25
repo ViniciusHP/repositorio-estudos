a68bead4094eb90fb9dc87bb4b304029f2eaf3e5
ViniciusHP <viniciushp97@gmail.com>
2021-12-25 13:58:55 -0300

    Declaração de variáveis

---
+++ alura/04-postgresql-desenvolva-com-pl-pgsql/09-declaracao-de-variaveis.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION primeira_pl() RETURNS INTEGER AS $$
+	DECLARE
+		primeira_variavel INTEGER DEFAULT 3;
+	BEGIN
+		primeira_variavel := primeira_variavel * 2;
+		RETURN primeira_variavel;
+	END
+$$ LANGUAGE plpgsql;
+
+SELECT primeira_pl();
