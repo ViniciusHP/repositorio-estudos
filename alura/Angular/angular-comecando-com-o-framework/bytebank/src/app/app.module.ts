52ed64c5ced48da3754bd62216e9571755bb6259
ViniciusHP <viniciushp97@gmail.com>
2022-02-25 21:29:29 -0300

    Aula Utilizando o primeiro componente

--- alura/Angular/angular-comecando-com-o-framework/bytebank/src/app/app.module.ts
+++ alura/Angular/angular-comecando-com-o-framework/bytebank/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponente } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponente
   ],
   imports: [
     BrowserModule
