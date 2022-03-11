933dc557be0a36aa4fbc0bc40b3dc8100454ec98
ViniciusHP <viniciushp97@gmail.com>
2022-03-10 21:33:45 -0300

    Aula Análise da tela de lista de animais

---
+++ alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { TokenService } from '../token.service';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  // Tipo de Subject que contém estado interno. Quando é feita uma inscrição, ele envia o último item
+  private usuarioSubject = new BehaviorSubject<Usuario>({});
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.possuiToken()) {
+      this.decodificaJWT();
+    }
+  }
+
+  retornaUsuario(): Observable<Usuario> {
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string) {
+    this.tokenService.salvaToken(token);
+    this.decodificaJWT();
+  }
+
+  logout(): void {
+    this.tokenService.excluiToken();
+    this.decodificaJWT();
+  }
+
+  estaLogado(): boolean {
+    return this.tokenService.possuiToken();
+  }
+
+  private decodificaJWT(): void {
+    const token = this.tokenService.retornaToken();
+
+    let usuario = {}
+
+    if(token !== '') {
+      usuario = jwtDecode<Usuario>(token);
+    }
+
+    this.usuarioSubject.next(usuario);
+  }
+}
