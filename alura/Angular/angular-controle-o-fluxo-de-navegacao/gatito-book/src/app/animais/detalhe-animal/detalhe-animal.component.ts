df357b3191578745c59ad55df9989fdfceb93e29
ViniciusHP <viniciushp97@gmail.com>
2022-03-13 14:43:38 -0300

    Aula Componente de detalhe

---
+++ alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from '../animal';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css'],
+})
+export class DetalheAnimalComponent implements OnInit {
+  animal$!: Observable<Animal>;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
