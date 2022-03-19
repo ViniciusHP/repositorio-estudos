b7ba86cc1324d936126fdc082ab2dd4ac49f8715
ViniciusHP <viniciushp97@gmail.com>
2022-03-19 14:53:34 -0300

    Aula Componente

--- alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, switchMap, tap } from 'rxjs';
 import { Comentarios } from './comentarios';
+import { ComentariosService } from './comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
@@ -8,9 +10,32 @@ import { Comentarios } from './comentarios';
   styleUrls: ['./comentarios.component.css'],
 })
 export class ComentariosComponent implements OnInit {
+  @Input() id!: number;
   comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
-  constructor() {}
+  constructor(
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.comentarios$ = this.comentariosService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
+  }
+
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentariosService
+      .incluiComentario(this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentariosService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert('Salvo comentário');
+        })
+      );
+  }
 }
