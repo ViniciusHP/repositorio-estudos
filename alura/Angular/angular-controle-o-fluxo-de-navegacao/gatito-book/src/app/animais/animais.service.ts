65b8f98a4a70fb46cbc22a5bf217fcaea1ad76f1
ViniciusHP <viniciushp97@gmail.com>
2022-03-20 11:04:32 -0300

    Aula Serviço de upload

--- alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/animais.service.ts
+++ alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/animais.service.ts
@@ -43,4 +43,20 @@ export class AnimaisService {
         })
       );
   }
+
+  upload(
+    descricao: string,
+    permiteComentario: boolean,
+    arquivo: File
+  ): Observable<any> {
+    const formData = new FormData();
+    formData.append('description', descricao);
+    formData.append('allowComments', permiteComentario ? 'true' : 'false');
+    formData.append('imageFile', arquivo);
+
+    return this.http.post(`${API}/photos/upload`, formData, {
+      observe: 'events',
+      reportProgress: true
+    });
+  }
 }
