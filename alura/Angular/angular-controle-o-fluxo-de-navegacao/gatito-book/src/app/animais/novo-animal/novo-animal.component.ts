7f62cd250cc4c969ad3a7c32a335de7433468369
ViniciusHP <viniciushp97@gmail.com>
2022-03-20 14:16:37 -0300

    Aula Componente de novo animal

--- alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/novo-animal/novo-animal.component.ts
+++ alura/Angular/angular-controle-o-fluxo-de-navegacao/gatito-book/src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,5 +1,10 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { read } from 'fs';
+import { finalize } from 'rxjs';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-novo-animal',
@@ -12,15 +17,47 @@ export class NovoAnimalComponent implements OnInit {
   preview!: string;
   percentualConcluido = 0;
 
-  constructor() {}
+  constructor(
+    private animaisService: AnimaisService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
-
-  upload() {
-
+  ngOnInit(): void {
+    this.formularioAnimal = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
 
-  gravaArquivo(arquivo: any) {
+  upload() {
+    const allowComments =
+      this.formularioAnimal.get('allowComments')?.value ?? false;
+    const description = this.formularioAnimal.get('description')?.value ?? '';
 
+    this.animaisService
+      .upload(description, allowComments, this.file)
+      .pipe(finalize(() => this.router.navigate(['animais'])))
+      .subscribe({
+        next: (event: HttpEvent<any>) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            const total = event.total ?? 1;
+            this.percentualConcluido = Math.round(100 * (event.loaded / total));
+          }
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  gravaArquivo(input: any): void {
+    const file = input?.files?.item(0) as File;
+
+    if (!file) return;
+
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
   }
 }
