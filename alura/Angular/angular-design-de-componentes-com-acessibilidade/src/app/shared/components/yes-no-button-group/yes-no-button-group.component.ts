40a2df03c198730ee14612546277c037e533de2b
ViniciusHP <viniciushp97@gmail.com>
2022-03-27 15:48:16 -0300

    Aula NG_VALUE_ACCESSOR

--- alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,10 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR, /* Provendo um ControlValueAccessor a partir de um InjectionToken */
+      multi: true, /* Durante a aplicação, podem haver vários provedores de ControlValueAccessor */
+      useExisting: forwardRef(() => YesNoButtonGroupComponent) /* Classe que provê a implementação do ControlValueAccessor */
+      /*
+        forwardRef é usado pois no momento que registramos o token, nossa classe ainda não existe. Dessa forma,
+        é feito o registro do token, mas o uso da classe só será feita de maneira tardia, quando a classe já estiver disponível
+      */
+    }
+  ]
 })
 export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
@@ -16,13 +27,16 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 
   constructor() {}
 
+  ngOnInit(): void {}
+
   public writeValue(value: string): void { // Mudança do modelo e necessidade de atualizar a view
     this.value = value;
     this.onChange(this.value);
+    this.valueChange.emit(this.value);
   }
 
   public registerOnChange(fn: (value: string) => void): void {
-    this.onChange = fn; // Função recebida do reactive forms module. A partir dela nós notificamos o formulário que houve uma mudaça no valor
+    this.onChange = fn; // Função recebida do reactive forms module. A partir dela nós notificamos o formulário que houve uma mudança no valor
   }
 
   public registerOnTouched(fn: () => void): void {
@@ -33,11 +47,8 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
     throw new Error('Method not implemented.');
   }
 
-  ngOnInit(): void {}
-
   public activate(value: string): void {
-    this.value = value;
-    this.valueChange.emit(this.value);
+    this.writeValue(value);
   }
 }
 
