1aa7c459bb20beacb83b5263a03eb59b5b13a439
ViniciusHP <viniciushp97@gmail.com>
2022-04-03 20:51:37 -0300

    Aula Implementando a lógica do teclado

--- alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -6,25 +6,46 @@ import { KeyboardManagedItemDirective } from "./keyboard-managed-item.directive"
 })
 export class KeyboardManagerDirective {
 
-  @ContentChildren(KeyboardManagedItemDirective) public items: QueryList<KeyboardManagedItemDirective>// = null
+  @ContentChildren(KeyboardManagedItemDirective) public items: QueryList<KeyboardManagedItemDirective> = null
 
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch(event.key) {
       case 'ArrowUp':
-        console.log(this.items);
-
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         console.log('up');
         break;
       case 'ArrowDown':
+        this.moveFocus(ArrowDirection.LEFT).focus();
         console.log('down');
         break;
       case 'ArrowLeft':
+        this.moveFocus(ArrowDirection.LEFT).focus();
         console.log('left');
         break;
       case 'ArrowRight':
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         console.log('right');
         break;
     }
   }
+
+  public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective {
+    const items = this.items.toArray();
+    const currentSelectedIndex = items.findIndex((item) => item.isFocused());
+    const targetElementFocus = items[currentSelectedIndex + direction];
+
+    if(targetElementFocus) {
+      return targetElementFocus;
+    }
+
+    return direction === ArrowDirection.LEFT
+      ? items[items.length - 1]
+      : items[0];
+  }
+}
+
+enum ArrowDirection {
+  LEFT = -1,
+  RIGHT = 1
 }
