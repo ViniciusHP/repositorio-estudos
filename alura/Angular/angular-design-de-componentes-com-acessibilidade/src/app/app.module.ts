c2f3b98e0cb66a22f05e7d2defc4ff1db5b3f0ee
ViniciusHP <viniciushp97@gmail.com>
2022-04-04 16:30:13 -0300

    Aula Disable Control Directive

--- alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/app.module.ts
+++ alura/Angular/angular-design-de-componentes-com-acessibilidade/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { DisableControlModule } from './shared/directives/disable-control/disable-control.module';
 import { AppComponent } from './app.component';
 import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     YesNoButtonGroupModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DisableControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
