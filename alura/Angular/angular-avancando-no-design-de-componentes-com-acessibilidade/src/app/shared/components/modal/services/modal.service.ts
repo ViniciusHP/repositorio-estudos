188514e08c1c0ed81397e8cd3f0baca40faabfb9
ViniciusHP <viniciushp97@gmail.com>
2022-04-10 16:10:24 -0300

    Aula AttachView

--- alura/Angular/angular-avancando-no-design-de-componentes-com-acessibilidade/src/app/shared/components/modal/services/modal.service.ts
+++ alura/Angular/angular-avancando-no-design-de-componentes-com-acessibilidade/src/app/shared/components/modal/services/modal.service.ts
@@ -1,5 +1,5 @@
 import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
-import { BodyInjectorService } from 'src/app/shared/services/body-injector';
+import { BodyInjectorService } from 'src/app/shared/services/body-injector.service';
 import { ModalConfig } from '../interfaces/modal-config';
 import { ModalComponent } from '../modal.component';
 
