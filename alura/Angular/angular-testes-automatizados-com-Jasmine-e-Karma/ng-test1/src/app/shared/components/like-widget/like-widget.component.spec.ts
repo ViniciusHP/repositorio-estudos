f6e1e0e9bc91796b54239c17ead31d808ff0d9a1
ViniciusHP <viniciushp97@gmail.com>
2022-04-23 16:32:29 -0300

    Aula O papel do TestBed

--- alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,14 +1,27 @@
-import { UniqueIdService } from "../../services/unique-id/unique-id.service";
+import { TestBed } from "@angular/core/testing";
 import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
     let component: LikeWidgetComponent = null;
 
-    beforeEach(() => {
-        component = new LikeWidgetComponent(new UniqueIdService());
+    /* Está forma é independente do builder do projeto */
+    beforeEach(async () => {
+        /* Vai esperar a compilação do componente para só depois dar continuidade aos testes */
+        await TestBed.configureTestingModule({
+            declarations: [LikeWidgetComponent]
+        }).compileComponents();
+        /* compileComponents é assíncrono, ele vai compilar o componente,
+        resolver o template do componente por meio de uma requisição assíncrona e gerar o componente */
     })
 
+    /* Se o builder for o Webpack, não é necessário fazer o processo assíncrono */
+    /* beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LikeWidgetComponent]
+        })
+    }) */
+
     it(``, () => {
-        
+
     })
 })
