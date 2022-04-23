db36f8285c9be6423ea1b6fca3cd50bda8bcf205
ViniciusHP <viniciushp97@gmail.com>
2022-04-23 11:05:11 -0300

    Aula  Dando 'nome aos bois'

--- alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,9 @@
-describe('O artefato que queremos testar', () => {
-    it('Primeira condição que queremos testar', () => {
+import { UniqueIdService } from "./unique-id.service";
 
-    });
-    
-    it('Segunda condição que queremos testar', () => {
-
+describe('UniqueIdService', () => {
+    it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+        const service = new UniqueIdService();
+        const id = service.generateUniqueIdWithPrefix('app');
+        expect(id).toContain('app-');
     });
 });
