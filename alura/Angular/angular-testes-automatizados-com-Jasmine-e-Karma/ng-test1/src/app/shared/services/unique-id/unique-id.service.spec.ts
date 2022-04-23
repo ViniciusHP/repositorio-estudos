6d2c6fd880e9754a6421b49494629fec38b1d109
ViniciusHP <viniciushp97@gmail.com>
2022-04-23 12:44:15 -0300

    Aaula Testando exceçõ

--- alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ alura/Angular/angular-testes-automatizados-com-Jasmine-e-Karma/ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,12 +7,16 @@ describe(UniqueIdService.name, () => {
         service = new UniqueIdService();
     });
 
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    SHOULD generate id
+    WHEN called with prefix`, () => {
         const id = service.generateUniqueIdWithPrefix('app');
         expect(id.startsWith('app-')).toBeTrue();
     });
 
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    SHOULD not generate duplicate IDs
+    WHEN called multiple times`, () => {
         const ids = new Set();
         for(let i = 0; i < 50; i++) {
             ids.add(service.generateUniqueIdWithPrefix('app'));
@@ -20,9 +24,20 @@ describe(UniqueIdService.name, () => {
         expect(ids.size).toBe(50);
     });
 
-    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated IDs when called`, () => {
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    SHOULD return the number of generated IDs
+    WHEN called`, () => {
         service.generateUniqueIdWithPrefix('app');
         service.generateUniqueIdWithPrefix('app');
         expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
     });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    SHOULD throw
+    WHEN called with empty`, () => {
+        const emptyValues = [null, undefined, ''];
+        emptyValues.forEach(emptyValue => {
+            expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+        })
+    })
 });
