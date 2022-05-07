4019d97ea9c68d209c7c727489f190324e0bb9ed
ViniciusHP <viniciushp97@gmail.com>
2022-05-07 14:54:39 -0300

    Aula Primeira surpresa

--- alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -18,4 +18,16 @@ describe(PhotoFrameComponent.name, () => {
   it(`SHOULD create component`, () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  SHOULD trigger (@Output liked) once
+  WHEN called multiple times within debounce time`, () => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+
+    expect(times).toBe(1);
+  });
 });
