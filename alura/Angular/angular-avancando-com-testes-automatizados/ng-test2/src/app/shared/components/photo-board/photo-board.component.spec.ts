5091502a97d87cc819c59e491d746797b15cfc94
ViniciusHP <viniciushp97@gmail.com>
2022-05-08 15:51:12 -0300

    Aula Mais surpresas ao testar componentes

---
+++ alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Photo } from './interfaces/photo';
+import { PhotoBoardComponent } from './photo-board.component';
+import { PhotoBoardModule } from './photo-board.module';
+
+function buildPhotoList(): Photo[] {
+  const photos: Photo[] = [];
+
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i,
+      url: '',
+      description: '',
+    });
+  }
+
+  return photos;
+}
+
+describe(PhotoBoardComponent.name, () => {
+  let fixture: ComponentFixture<PhotoBoardComponent> = null;
+  let component: PhotoBoardComponent = null;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoBoardModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PhotoBoardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it(`SHOULD display rows and columns
+  WHEN (@Input photos) has value`, () => {
+    component.photos = buildPhotoList();
+    fixture.detectChanges();
+    expect(component.rows.length)
+      .withContext(`Number of rows`)
+      .toBe(2);
+    expect(component.rows[0].length)
+      .withContext(`Number of columns from the first row`)
+      .toBe(4);
+    expect(component.rows[1].length)
+      .withContext(`Number of columns from the second row`)
+      .toBe(4);
+  });
+});
