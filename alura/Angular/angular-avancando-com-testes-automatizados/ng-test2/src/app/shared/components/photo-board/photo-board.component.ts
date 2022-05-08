f67396d77c72829360e8cc1a5ced9ec3982afcee
ViniciusHP <viniciushp97@gmail.com>
2022-05-08 13:40:00 -0300

    Aula Ngonchanges e lógica especial

--- alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-board/photo-board.component.ts
+++ alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-board/photo-board.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from './interfaces/photo';
 
 @Component({
   selector: 'app-photo-board',
   templateUrl: './photo-board.component.html',
-  styleUrls: ['./photo-board.component.scss']
+  styleUrls: ['./photo-board.component.scss'],
 })
-export class PhotoBoardComponent {
+export class PhotoBoardComponent implements OnChanges {
   @Input() public photos: Photo[];
   public rows: any[][] = [];
 
-  
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupColumns(changes.photos.currentValue);
+    }
+  }
+
+  public groupColumns(photos: Photo[]): any[][] {
+    const newRows = [];
+    const step = 4;
+
+    for (let index = 0; index < photos.length; index += step) {
+      newRows.push(photos.slice(index, index + step));
+    }
+
+    return newRows;
+  }
 }
