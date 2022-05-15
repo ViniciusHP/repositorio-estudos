e4de562caa37a5d444c3c61ed1eba69b63498bee
ViniciusHP <viniciushp97@gmail.com>
2022-05-15 14:26:30 -0300

    Aula Testando chamadas às APIs

--- alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-board/services/photo-board.service.spec.ts
+++ alura/Angular/angular-avancando-com-testes-automatizados/ng-test2/src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -1,19 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { PhotoBoardService } from './photo-board.service';
 
-describe(PhotoBoardService.name, () => {
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: ''
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: ''
+    },
+  ]
+}
 
+describe(PhotoBoardService.name, () => {
   let service: PhotoBoardService = null;
   let httpController: HttpTestingController = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [PhotoBoardService]
+      providers: [PhotoBoardService],
     });
 
     service = TestBed.inject(PhotoBoardService);
     httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it(`#${PhotoBoardService.prototype.getPhotos.name}
+  SHOULD return photos with description in uppercase`, (done) => {
+    service.getPhotos().subscribe(photos => {
+      expect(photos[0].description).toBe('EXAMPLE 1');
+      expect(photos[1].description).toBe('EXAMPLE 2');
+      done();
+    });
+
+    // A configuração deve ficar depois do subscribe
+    // pois a requisição deve ser feita primeiro para só depois configurar qual a resposta para ela
+    // até lá, a requisição estará parada
+    httpController
+      .expectOne(mockData.api)
+      .flush(mockData.data);
   });
 });
