0d45bae79b8e8f8002d2f7fb5aa07a4bb712e4c1
ViniciusHP <viniciushp97@gmail.com>
2022-05-21 14:31:05 -0300

    Aula Operador Map

--- alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.service.ts
+++ alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,14 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AcoesService {
-
   constructor(private httpClient: HttpClient) {}
 
   getAcoes() {
-    return this.httpClient.get<any>(`http://localhost:3000/acoes`);
+    return this.httpClient
+      .get<any>(`http://localhost:3000/acoes`)
+      .pipe(
+        map((acoes) =>
+          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+        )
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao): number {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
