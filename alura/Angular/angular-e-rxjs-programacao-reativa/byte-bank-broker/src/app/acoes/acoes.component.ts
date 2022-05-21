e8a425701bfa7347625d9b060264bf9286c7d76d
ViniciusHP <viniciushp97@gmail.com>
2022-05-21 15:54:12 -0300

    Aula Merge

--- alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.component.ts
+++ alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { merge, Observable } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
 import { Acoes } from './modelo/acoes';
@@ -12,15 +12,21 @@ import { Acoes } from './modelo/acoes';
 })
 export class AcoesComponent implements OnInit {
   acoesInput = new FormControl();
+  todasAcoes$: Observable<Acoes>;
+  filtroPeloInput$: Observable<Acoes>;
   acoes$: Observable<Acoes>;
 
   constructor(private acoesService: AcoesService) {}
 
   ngOnInit(): void {
-    this.acoes$ = this.acoesInput.valueChanges.pipe(
-      tap(console.log),
-      switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
-      tap(console.log)
+    this.todasAcoes$ = this.acoesService.getAcoes()
+      .pipe(tap(() => console.log('Fluxo inicial')));
+
+    this.filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+      tap(() => console.log('Fluxo filtro')),
+      switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
     );
+
+    this.acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
   }
 }
