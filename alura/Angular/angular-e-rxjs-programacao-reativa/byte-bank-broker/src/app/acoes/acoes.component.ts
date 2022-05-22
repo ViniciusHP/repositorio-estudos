d3fcbce8567d083e4af0f1218c53522ac61afb90
ViniciusHP <viniciushp97@gmail.com>
2022-05-22 10:55:53 -0300

    Aula DistinctUntilChanged

--- alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.component.ts
+++ alura/Angular/angular-e-rxjs-programacao-reativa/byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { merge, Observable } from 'rxjs';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
 import { Acoes } from './modelo/acoes';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -25,9 +27,11 @@ export class AcoesComponent implements OnInit {
 
     this.filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
       tap(() => console.log('Fluxo filtro')),
+      debounceTime(ESPERA_DIGITACAO),
       filter(
         (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
       ),
+      distinctUntilChanged(),
       switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
     );
 
