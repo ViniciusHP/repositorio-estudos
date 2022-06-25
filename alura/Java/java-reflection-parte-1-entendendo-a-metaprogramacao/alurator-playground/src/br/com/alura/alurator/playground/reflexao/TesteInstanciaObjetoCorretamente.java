d31797ac98273c48284436d8417aac94b2f7985d
ViniciusHP <viniciushp97@gmail.com>
2022-06-25 12:52:52 -0300

    Aula A vantagem do Constructor.newInstance()

--- alura/Java/java-reflection-parte-1-entendendo-a-metaprogramacao/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjetoCorretamente.java
+++ alura/Java/java-reflection-parte-1-entendendo-a-metaprogramacao/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjetoCorretamente.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class TesteInstanciaObjetoCorretamente {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Class<SubControle> subControleClasse1 = SubControle.class;
 
         Class<?> subControleClasse2 =
@@ -15,9 +15,16 @@ public class TesteInstanciaObjetoCorretamente {
         Class<?> controleClasse1 =
                 Class.forName("br.com.alura.alurator.playground.controle.Controle");
 
+
+
         Constructor<SubControle> construtorSubControle = subControleClasse1.getDeclaredConstructor();
         construtorSubControle.setAccessible(true);
-        Object subControle = construtorSubControle.newInstance();
-        System.out.println(subControle);
+
+        try {
+            Object subControle = construtorSubControle.newInstance();
+            System.out.println(subControle);
+        } catch (InvocationTargetException e) {
+            System.out.println(e.getTargetException().toString());
+        }
     }
 }
