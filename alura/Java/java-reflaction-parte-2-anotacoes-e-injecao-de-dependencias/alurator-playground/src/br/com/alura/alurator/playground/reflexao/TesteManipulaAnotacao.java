3b5a11a3ff77b832d3c388628f3f53ddaa946af5
ViniciusHP <viniciushp97@gmail.com>
2022-07-30 17:06:21 -0300

    Aula Anotações e reflexão

---
+++ alura/Java/java-reflaction-parte-2-anotacoes-e-injecao-de-dependencias/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteManipulaAnotacao.java
@@ -0,0 +1,16 @@
+package br.com.alura.alurator.playground.reflexao;
+
+import br.com.alura.alurator.playground.anotacao.NomeTagXML;
+import br.com.alura.alurator.playground.modelo.Produto;
+
+public class TesteManipulaAnotacao {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto("Produto 1", 20.0, "Marca 1");
+        Class<?> classe = produto.getClass();
+
+        NomeTagXML declaredAnnotation = classe.getDeclaredAnnotation(NomeTagXML.class);
+        String valor = declaredAnnotation.value();
+        System.out.println(valor);
+    }
+}
