2a1b252d0074e015d0a6735f97c2ac361ca4e44e
ViniciusHP <viniciushp97@gmail.com>
2022-08-21 14:02:33 -0300

    Aula Removendo tópicos

--- alura/Java/spring-boot-api-rest-construa-uma-api/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ alura/Java/spring-boot-api-rest-construa-uma-api/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -36,8 +36,18 @@ public class TopicosController {
         return TopicoDTO.converter(topicos);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id) {
+        Optional<Topico> optionalTopico = topicoRepository.findById(id);
+
+        return optionalTopico
+                .map(topico -> ResponseEntity.ok(new DetalhesDoTopicoDTO(topico)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
 
     @PostMapping()
+    @Transactional
     public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topico = topicoRepository.save(topico);
@@ -49,16 +59,6 @@ public class TopicosController {
         return ResponseEntity.created(uri).body(new TopicoDTO(topico));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id) {
-        Optional<Topico> optionalTopico = topicoRepository.findById(id);
-
-        return optionalTopico
-                .map(topico -> ResponseEntity.ok(new DetalhesDoTopicoDTO(topico)))
-                .orElseGet(() -> ResponseEntity.notFound().build());
-
-    }
-
     @PutMapping("/{id}")
     @Transactional
     public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
@@ -67,5 +67,12 @@ public class TopicosController {
         return topico == null
                 ? ResponseEntity.notFound().build()
                 : ResponseEntity.ok(topico);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
