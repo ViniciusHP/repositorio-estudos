36c4ae77bcb1d0ee3994ee8986d764f3021a66aa
ViniciusHP <viniciushp97@gmail.com>
2022-08-21 12:17:39 -0300

    Aula Validações com Bean Validation

--- alura/Java/spring-boot-api-rest-construa-uma-api/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ alura/Java/spring-boot-api-rest-construa-uma-api/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -3,12 +3,14 @@ package br.com.alura.forum.controller.form;
 import br.com.alura.forum.controller.repository.CursoRepository;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.TopicoRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Optional;
 
 @Getter
@@ -16,8 +18,14 @@ import java.util.Optional;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TopicoForm {
+
+    @NotNull @NotEmpty @Size(min = 5)
     private String titulo;
+
+    @NotNull @NotEmpty @Size(min = 10)
     private String mensagem;
+
+    @NotNull @NotEmpty
     private String nomeCurso;
 
     public Topico converter(CursoRepository repository) {
