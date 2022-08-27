1dcb728bed74933846510de784935dbef9ca65cd
ViniciusHP <viniciushp97@gmail.com>
2022-08-27 15:55:19 -0300

    Aula Liberando acesso aos endpoints públicos

--- alura/Java/spring-boot-api-rest-seguranca-de-api-cache-e-monitor/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ alura/Java/spring-boot-api-rest-seguranca-de-api-cache-e-monitor/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,11 +1,45 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
-    
+
+    /*WebSecurityConfigurerAdapter*/
+    /* Configurações de autenticação */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    /* Configurações de autorização */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    /* Configurações de recursos estáticos (js, css, imagens, etc.) */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+   /* @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }*/
 }
