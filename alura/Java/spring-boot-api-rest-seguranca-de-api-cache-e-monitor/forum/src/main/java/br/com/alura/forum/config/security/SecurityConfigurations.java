c40df7ae728493f19c5a35c9c70897fd56e72f79
ViniciusHP <viniciushp97@gmail.com>
2022-08-28 10:17:15 -0300

    Aula Recuperando o token do header Authorization

--- alura/Java/spring-boot-api-rest-seguranca-de-api-cache-e-monitor/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ alura/Java/spring-boot-api-rest-seguranca-de-api-cache-e-monitor/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -38,7 +39,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     /* Configurações de recursos estáticos (js, css, imagens, etc.) */
