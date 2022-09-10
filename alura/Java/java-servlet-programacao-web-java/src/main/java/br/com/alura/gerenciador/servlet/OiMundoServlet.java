dab8b97463b3f48bc7b629581f779a38f9269431
ViniciusHP <viniciushp97@gmail.com>
2022-09-10 12:14:57 -0300

    Aula Finalmente, o primeiro servlet

---
+++ alura/Java/java-servlet-programacao-web-java/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,30 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		
+		out.write(
+		"""
+		<html>
+			<body>
+					Oi mundo, parabéns você escreveu o primeiro servlet.
+			</body>
+		</html>
+		""");
+		
+		System.out.println("O Servlet OiMundoServlet foi chamado!");
+		
+	}
+}
