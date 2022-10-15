082ae3b10cbc3fbaf4ce821f325b154b64a522ca
ViniciusHP <viniciushp97@gmail.com>
2022-10-15 15:52:37 -0300

    Aula Liremoção

--- alura/Java/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja_virtual/src/main/java/br/com/vhp/Remocao.java
+++ alura/Java/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja_virtual/src/main/java/br/com/vhp/Remocao.java
@@ -1,15 +1,13 @@
 package br.com.vhp;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Remocao {
     public static void main(String[] args) throws SQLException {
         Connection con = ConexaoFactory.recuperarConexao();
-        Statement stm = con.createStatement();
-        stm.execute("DELETE FROM PRODUTO WHERE ID = 4");
+        PreparedStatement stm = con.prepareStatement("DELETE FROM PRODUTO WHERE ID = ?");
+        stm.setInt(1, 4);
+        stm.execute();
         Integer linhasApagadas = stm.getUpdateCount();
         System.out.println("Quantidade de linhas que foram apagadas: " + linhasApagadas);
         con.close();
