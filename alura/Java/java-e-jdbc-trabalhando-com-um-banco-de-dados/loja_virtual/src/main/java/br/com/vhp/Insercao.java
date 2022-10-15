a5e846ce01367f0a685bcf717ed0d8605d390624
ViniciusHP <viniciushp97@gmail.com>
2022-10-15 15:31:10 -0300

    Aula Inserindo com Statement

---
+++ alura/Java/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja_virtual/src/main/java/br/com/vhp/Insercao.java
@@ -0,0 +1,24 @@
+package br.com.vhp;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Insercao {
+
+    public static void main(String[] args) throws SQLException {
+        Connection con = ConexaoFactory.recuperarConexao();
+        Statement stm = con.createStatement();
+        stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('MOUSE', 'MOUSE SEM FIO')",
+                Statement.RETURN_GENERATED_KEYS);
+
+        ResultSet rs = stm.getGeneratedKeys();
+
+        while(rs.next()) {
+            int id = rs.getInt(1);
+            System.out.println("O id criado foi: " + id);
+        }
+        con.close();
+    }
+}
