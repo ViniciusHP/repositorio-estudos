848189dc595636b24b16cbb5da345acf9229da39
ViniciusHP <viniciushp97@gmail.com>
2022-11-13 12:54:36 -0300

    aula Gerando o banco de dados

---
+++ alura/Java/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -0,0 +1,20 @@
+package br.com.alura.spring.data.orm;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cargos")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+}
