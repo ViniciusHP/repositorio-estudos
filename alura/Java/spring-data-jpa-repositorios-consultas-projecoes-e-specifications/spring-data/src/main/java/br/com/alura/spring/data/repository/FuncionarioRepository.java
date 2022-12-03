852916615b16d776116974524e57f30bf097e5d9
ViniciusHP <viniciushp97@gmail.com>
2022-12-03 16:49:25 -0300

    Aula Usando JPQL

--- alura/Java/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ alura/Java/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,13 +1,26 @@
 package br.com.alura.spring.data.repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
 
     List<Funcionario> findByNomeLike(String nome);
+
+    @Query("""
+        SELECT f FROM
+        Funcionario f
+        WHERE f.nome = :nome
+            AND f.salario >= :salario
+            AND f.data_contratacao = :dataContratacao
+    """)
+    List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, BigDecimal salario, LocalDate dataContratacao);
+    List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, BigDecimal salario, LocalDate dataContratacao);
 }
