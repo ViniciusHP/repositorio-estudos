ba5a5cfe79cf296141ecee820c0974f0716259fb
ViniciusHP <viniciushp97@gmail.com>
2022-12-17 15:17:23 -0300

    Aula Usando JDBC Authentication

--- alura/Java/spring-mvc-autenticacao-com-spring-security-api-rest-e-ajax/mudi/src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
+++ alura/Java/spring-mvc-autenticacao-com-spring-security-api-rest-e-ajax/mudi/src/main/java/br/com/alura/mvc/mudi/config/WebSecurityConfig.java
@@ -4,16 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  {
+public class WebSecurityConfig {
+
+    private final static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,20 +26,47 @@ public class WebSecurityConfig  {
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.loginPage("/login").permitAll())
+                .formLogin(form ->
+                        form.loginPage("/login")
+                            .defaultSuccessUrl("/home", true)
+                            .permitAll())
                 .logout(logout -> logout.logoutUrl("/logout").permitAll())
                 .build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("vi")
-                        .password("vi")
-                        .roles("ADM")
-                        .build();
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("vi")
+//                        .password("vi")
+//                        .roles("ADM")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(encoder.encode("user"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("admin"))
+//                .roles("USER", "ADMIN")
+//                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+//        users.createUser(user);
+//        users.createUser(admin);
+        return users;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return encoder;
+    }
+
 }
