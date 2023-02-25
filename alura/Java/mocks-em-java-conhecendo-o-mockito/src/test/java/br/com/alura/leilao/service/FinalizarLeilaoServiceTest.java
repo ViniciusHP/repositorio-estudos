1fdbc58615a8f5ebe4c3752c283bb550b233e213
ViniciusHP <viniciushp97@gmail.com>
2023-02-25 15:29:04 -0300

    Aula Verificando comportamentos em casos de exceptions

--- alura/Java/mocks-em-java-conhecendo-o-mockito/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ alura/Java/mocks-em-java-conhecendo-o-mockito/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -62,6 +62,22 @@ public class FinalizarLeilaoServiceTest {
         Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);
     }
 
+    @Test
+    void naoDeveriaEnviarEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarOLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when(leilaoDao.buscarLeiloesExpirados())
+                .thenReturn(leiloes);
+
+        Mockito.when(leilaoDao.salvar(Mockito.any()))
+                        .thenThrow(RuntimeException.class);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            service.finalizarLeiloesExpirados();
+        });
+        Mockito.verifyNoInteractions(enviadorDeEmails);
+    }
+
     private List<Leilao> leiloes() {
         List<Leilao> lista = new ArrayList<>();
 
