83c06ab447b6c68a92622760233bfb264fa145e6
ViniciusHP <viniciushp97@gmail.com>
2023-02-25 16:15:04 -0300

    aula Lidando com métodos estáticos

--- alura/Java/mocks-em-java-conhecendo-o-mockito/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ alura/Java/mocks-em-java-conhecendo-o-mockito/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -11,10 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.*;
 
 public class GeradorDePagamentoTest {
 
@@ -23,25 +20,90 @@ public class GeradorDePagamentoTest {
     @Mock
     private PagamentoDao pagamentoDao;
 
+    @Mock
+    private Clock clock;
+
     @Captor
     private ArgumentCaptor<Pagamento> captor;
 
     @BeforeEach
     void beforeEach() {
         MockitoAnnotations.initMocks(this);
-        gerador = new GeradorDePagamento(pagamentoDao);
+        gerador = new GeradorDePagamento(pagamentoDao, clock);
     }
 
     @Test
     void deveriaCriarPagamentoParaVencedorDoLeilao() {
         Leilao leilao = getLeilao();
         Lance vencedor = leilao.getLanceVencedor();
+
+        LocalDate dataAtual = LocalDate.of(2022, 10, 31);
+        Instant intant = dataAtual.atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+
+        Mockito.when(clock.instant())
+                .thenReturn(intant);
+        Mockito.when(clock.getZone())
+                        .thenReturn(ZoneId.systemDefault());
+
         gerador.gerarPagamento(vencedor);
 
         Mockito.verify(pagamentoDao).salvar(captor.capture());
         Pagamento pagamento = captor.getValue();
 
-        Assertions.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+        Assertions.assertEquals(dataAtual.plusDays(1), pagamento.getVencimento());
+        Assertions.assertEquals(vencedor.getValor(), pagamento.getValor());
+        Assertions.assertFalse(pagamento.getPago());
+        Assertions.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+        Assertions.assertEquals(vencedor.getLeilao(), pagamento.getLeilao());
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilaoNaSegundaQuandoVencimentoSerSabado() {
+        Leilao leilao = getLeilao();
+        Lance vencedor = leilao.getLanceVencedor();
+
+        LocalDate dataAtual = LocalDate.of(2022, 10, 29);
+        Instant intant = dataAtual.atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+
+        Mockito.when(clock.instant())
+                .thenReturn(intant);
+        Mockito.when(clock.getZone())
+                .thenReturn(ZoneId.systemDefault());
+
+        gerador.gerarPagamento(vencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+        Pagamento pagamento = captor.getValue();
+
+        Assertions.assertEquals(dataAtual.plusDays(2), pagamento.getVencimento());
+        Assertions.assertEquals(vencedor.getValor(), pagamento.getValor());
+        Assertions.assertFalse(pagamento.getPago());
+        Assertions.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+        Assertions.assertEquals(vencedor.getLeilao(), pagamento.getLeilao());
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilaoNaSegundaQuandoVencimentoSerDomingo() {
+        Leilao leilao = getLeilao();
+        Lance vencedor = leilao.getLanceVencedor();
+
+        LocalDate dataAtual = LocalDate.of(2022, 10, 30);
+        Instant intant = dataAtual.atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+
+        Mockito.when(clock.instant())
+                .thenReturn(intant);
+        Mockito.when(clock.getZone())
+                .thenReturn(ZoneId.systemDefault());
+
+        gerador.gerarPagamento(vencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+        Pagamento pagamento = captor.getValue();
+
+        Assertions.assertEquals(dataAtual.plusDays(1), pagamento.getVencimento());
         Assertions.assertEquals(vencedor.getValor(), pagamento.getValor());
         Assertions.assertFalse(pagamento.getPago());
         Assertions.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
