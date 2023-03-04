a8ef0288f2c7d62aa3f5b2ee9da4cfee79aef79f
ViniciusHP <viniciushp97@gmail.com>
2023-03-04 15:09:56 -0300

    Aula Abstract factory

--- cod3r/padroes-de-projeto-em-java-na-pratica/creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ cod3r/padroes-de-projeto-em-java-na-pratica/creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,27 @@
 package br.com.cod3r.abstractfactory.apple;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.USRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
 
-	}
+public class Client {
+
+    public static void main(String[] args) {
+//        CountryRulesAbstractFactory rules = new BrazilianRulesAbstractFactory();
+        CountryRulesAbstractFactory rules = new USRulesAbstractFactory();
+        IPhoneFactory iphone11Factory = new IPhone11Factory(rules);
+        IPhoneFactory iphoneXFactory = new IPhoneXFactory(rules);
+
+        System.out.println("### Ordering an iPhone X");
+        IPhone iphoneX = iphoneXFactory.orderIPhone("standard");
+        System.out.println(iphoneX);
+
+        System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+        IPhone iphone11 = iphone11Factory.orderIPhone("highEnd");
+        System.out.println(iphone11);
+    }
 }
