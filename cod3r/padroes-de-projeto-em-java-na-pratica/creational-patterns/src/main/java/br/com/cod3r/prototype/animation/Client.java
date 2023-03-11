6efdd58d608e0fa847e81018f059e3bb6357e2ad
ViniciusHP <viniciushp97@gmail.com>
2023-03-11 15:04:54 -0300

    Aula prototype

--- cod3r/padroes-de-projeto-em-java-na-pratica/creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
+++ cod3r/padroes-de-projeto-em-java-na-pratica/creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.cod3r.prototype.animation.model.Person;
+import br.com.cod3r.prototype.animation.model.PersonSamples;
 
 public class Client {
 	private static List<Person> frames = new ArrayList<Person>();
@@ -26,7 +27,50 @@ public class Client {
 		System.out.print("**********************");
 	}
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws CloneNotSupportedException, InterruptedException {
+		PersonSamples personSamples = new PersonSamples();
+		Person person = personSamples.get("fatMan");
+		addFrame(person);
+
+		person = moveTo(person, "left");
+		addFrame(person);
+
+		person = moveTo(person, "left");
+		addFrame(person);
+
+		person = moveTo(person, "left");
+		addFrame(person);
+
+		person = moveTo(person, "left");
+		addFrame(person);
+
+		person = moveTo(person, "right");
+		addFrame(person);
+
+		person = moveTo(person, "right");
+		addFrame(person);
+
+		person = moveTo(person, "right");
+		addFrame(person);
+
+		person = moveTo(person, "right");
+		addFrame(person);
+
+		animate();
+	}
+
+	private static void addFrame(Person person) {
+		frames.add(person);
+	}
+
+	private static Person moveTo(Person person, String side) throws CloneNotSupportedException {
+		Person personClone = (Person) person.clone();
+
+		if("left".equalsIgnoreCase(side)) {
+			personClone.left();
+		} else if ("right".equalsIgnoreCase(side)) {
+			personClone.right();
+		}
+		return personClone;
 	}
 }
