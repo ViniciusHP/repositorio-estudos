edc5aa51a678fc252927f0fcdbba0698accc68ef
ViniciusHP <viniciushp97@gmail.com>
2023-07-15 15:17:50 -0300

    Implementando outros métodos

--- alura/Java/design-patters-em-java-II/src/br/com/alura/loja/orcamento/OrcamentoProxy.java
+++ alura/Java/design-patters-em-java-II/src/br/com/alura/loja/orcamento/OrcamentoProxy.java
@@ -1,11 +1,14 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.Finalizado;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class OrcamentoProxy extends Orcamento{
 
     private BigDecimal valor;
-    private Orcamento orcamento;
+    private final Orcamento orcamento;
 
     public OrcamentoProxy(Orcamento orcamento) {
         this.orcamento = orcamento;
@@ -18,5 +21,50 @@ public class OrcamentoProxy extends Orcamento{
         }
 
         return valor;
+    }
+
+    @Override
+    public void aplicarDescontoExtra() {
+        orcamento.aplicarDescontoExtra();
+    }
+
+    @Override
+    public void aprovar() {
+        orcamento.aprovar();
+    }
+
+    @Override
+    public void reprovar() {
+        orcamento.reprovar();
+    }
+
+    @Override
+    public void finalizar() {
+        orcamento.finalizar();
+    }
+
+    @Override
+    public int getQuantidadeItens() {
+        return orcamento.getQuantidadeItens();
+    }
+
+    @Override
+    public SituacaoOrcamento getSituacao() {
+        return orcamento.getSituacao();
+    }
+
+    @Override
+    public void setSituacao(SituacaoOrcamento situacao) {
+        orcamento.setSituacao(situacao);
+    }
+
+    @Override
+    public boolean isFinalizado() {
+        return orcamento.isFinalizado();
+    }
+
+    @Override
+    public void adicionarItem(Orcavel item) {
+        orcamento.adicionarItem(item);
     }
 }
