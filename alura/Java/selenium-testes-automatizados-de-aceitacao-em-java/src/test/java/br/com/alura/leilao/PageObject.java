2cead0bdf37564f55d34e3dfa50a687ccf66b1ce
ViniciusHP <viniciushp97@gmail.com>
2023-09-10 18:33:16 -0300

    Aula Teste de requisições assíncronas

--- alura/Java/selenium-testes-automatizados-de-aceitacao-em-java/src/test/java/br/com/alura/leilao/PageObject.java
+++ alura/Java/selenium-testes-automatizados-de-aceitacao-em-java/src/test/java/br/com/alura/leilao/PageObject.java
@@ -3,6 +3,8 @@ package br.com.alura.leilao;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class PageObject {
 
     protected WebDriver browser;
@@ -10,6 +12,12 @@ public class PageObject {
     public PageObject(WebDriver browser) {
         this.browser = browser == null ? new ChromeDriver() : browser;
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+
+        this.browser.manage()
+                .timeouts()
+                .implicitlyWait(5, TimeUnit.SECONDS) // Tempo de espera para buscar elemento na página até lançar exception
+                .pageLoadTimeout(10, TimeUnit.SECONDS);
     }
 
     public void fechar() {
