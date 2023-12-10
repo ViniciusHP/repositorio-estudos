60efbd1ee009b821a23eb48962ab58363274817c
ViniciusHP <viniciushp97@gmail.com>
2023-12-10 20:47:59 -0300

    Aula Novo cenário

--- alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -7,9 +7,6 @@ import org.junit.Assert;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
@@ -18,16 +15,19 @@ public class PropondoLanceSteps {
 	
 	private Lance lance;
 	private Leilao leilao;
+	
+	private Lance lance10;
+	private Lance lance15;
 
 	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("Fulano");
 		lance = new Lance(usuario, BigDecimal.TEN);
+		leilao = new Leilao("Tablet XPTO");
 	}
 
-	@Quando("propõe o lance")
+	@Quando("propõe ao leilão")
 	public void quando_propõe_o_lance() {
-		leilao = new Leilao("Tablet XPTO");
 		leilao.propoe(lance);
 	}
 
@@ -36,4 +36,29 @@ public class PropondoLanceSteps {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
+	
+
+	@Dado("vários lances válidos")
+	public void vários_lances_válidos() {
+		Usuario usuario1 = new Usuario("Fulano");
+		lance10 = new Lance(usuario1, BigDecimal.TEN);
+		
+		Usuario usuario2 = new Usuario("Beltrano");
+		lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+		leilao = new Leilao("Tablet XPTO");
+	}
+
+	@Quando("propõe vários lances ao leilão")
+	public void propõe_vários_lances_ao_leilão() {
+		leilao.propoe(lance10);
+		leilao.propoe(lance15);
+	}
+	
+	@Entao("os lances são aceitos")
+	public void os_lances_são_aceitos() {
+		Assert.assertEquals(2, leilao.getLances().size());
+		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+		Assert.assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
+	}
+
 }
--- alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/resources/features/propondo_lances.feature
+++ alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/resources/features/propondo_lances.feature
@@ -1,7 +1,12 @@
 # language: pt
-Funcionalidade: Pronpondo lances
+Funcionalidade: Pronpondo lances ao leilão
 
 Cenário: Propondo um único lance válido
  Dado um lance valido
- Quando propõe o lance
+ Quando propõe ao leilão
  Então o lance é aceito
+ 
+Cenário: Propondo vários lances válidos
+ Dado vários lances válidos
+ Quando propõe vários lances ao leilão
+ Então os lances são aceitos
