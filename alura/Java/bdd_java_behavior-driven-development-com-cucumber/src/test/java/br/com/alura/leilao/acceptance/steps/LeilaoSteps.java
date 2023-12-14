5cb74b7adb5393f625abe6d5e5105169e7105405
ViniciusHP <viniciushp97@gmail.com>
2023-12-13 22:09:10 -0300

    Aula Tags e Novo Leilão

---
+++ alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,28 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LeilaoSteps {
+
+	@Dado("o usuário logado")
+	public void o_usuário_logado() {
+	}
+
+	@Quando("acessa a página de novo leilão")
+	public void acessa_a_página_de_novo_leilão() {
+	}
+	
+	@Quando("preenche o formulário com dados válidos")
+	public void preenche_o_formulário_com_dados_válidos() {
+	}
+	
+	@Entao("volta para a página de leilões")
+	public void volta_para_a_página_de_leilões() {
+	}
+	
+	@Entao("o novo leilão aparece na tabela")
+	public void o_novo_leilão_aparece_na_tabela() {
+	}
+}
---
+++ alura/Java/bdd_java_behavior-driven-development-com-cucumber/src/test/resources/features/leilao.feature
@@ -0,0 +1,11 @@
+# language: pt
+Funcionalidade: Cadastrando um leilão
+
+@leilao
+Cenário: Um usuário logado pode cadastrar um leilão
+Dado o usuário logado
+Quando acessa a página de novo leilão
+E preenche o formulário com dados válidos
+Então volta para a página de leilões
+E o novo leilão aparece na tabela 
+
