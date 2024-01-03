39e5449ef0fbcc06722f3b3581e04cdf780d8682
ViniciusHP <viniciushp97@gmail.com>
2024-01-02 21:50:15 -0300

    Aula Adicionando telefones

--- alura/Java/java-clean-code-architecture-descomplicando-arquitetura-de-software/escola/src/main/java/br/com/alura/Aluno.java
+++ alura/Java/java-clean-code-architecture-descomplicando-arquitetura-de-software/escola/src/main/java/br/com/alura/Aluno.java
@@ -1,7 +1,15 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
     private String cpf;
     private String nome;
     private Email email;
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public void adicionarTelefone(String ddd, String numero) {
+        telefones.add(new Telefone(ddd, numero));
+    }
 }
