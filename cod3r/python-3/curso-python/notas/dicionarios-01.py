53a849755a28d19f2ab7260d4c083e56d588c8e2
ViniciusHP <viniciushp97@gmail.com>
2024-01-18 20:45:17 -0300

    Aula dicionários #02

--- cod3r/python-3/curso-python/notas/dicionarios-01.py
+++ cod3r/python-3/curso-python/notas/dicionarios-01.py
@@ -1,16 +1,30 @@
 # Dicionário
 nomeChave = 'chave'
 
-pessoa = {'nome': 'Prof(a). Ana', nomeChave: 38, 'cursos': [
+pessoa = {'nome': 'Prof(a). Ana', 'idade': 38, nomeChave: 38, 'cursos': [
     'Inglês', 'Português']}
 print(type(pessoa))
 print(len(pessoa))
 print(pessoa['nome'])
 print(pessoa['cursos'][1])
 # print(pessoa['tags'])
+
 print(pessoa.keys())
 print(pessoa.values())
 print(pessoa.items())
+
 print(pessoa.get('cursos'))
 print(pessoa.get('tags'))
 print(pessoa.get('tags', []))
+
+pessoa['idade'] = 44
+pessoa['cursos'].append('Angular')
+print(pessoa.pop('idade'))
+print(pessoa)
+pessoa.update({'idade': 40, 'Sexo': 'M'})
+
+del pessoa['cursos']
+print(pessoa)
+
+pessoa.clear()
+print(pessoa)
