fcc77a291b744b30a113b8300a58ce281cd358fd
ViniciusHP <viniciushp97@gmail.com>
2024-01-18 15:12:18 -0300

    aula mais operadores

--- cod3r/python-3/curso-python/notas/desafio-operadores-logicos.py
+++ cod3r/python-3/curso-python/notas/desafio-operadores-logicos.py
@@ -49,3 +49,26 @@ esta_chovendo = False
 print('Hoje estou com as roupas ' + ('secas.', 'molhadas.')[esta_chovendo])
 print('Hoje estou com as roupas ' + ('molhadas.'
                                      if esta_chovendo else 'secas.'))
+
+# Mais Operadores
+
+# Operador de Membro
+lista = [1, 2, 3, 'Ana', 'Carla']
+print(2 in lista)
+print('Ana' not in lista)
+
+# Operador de Identidade
+x = 3
+y = x
+z = 3
+print(x is y)
+print(y is z)
+print(x is not z)
+
+lista_a = [1, 2, 3]
+lista_b = lista_a
+lista_c = [1, 2, 3]
+
+print(lista_a is lista_b)
+print(lista_b is lista_c)
+print(lista_a is not lista_c)
