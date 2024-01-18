78ee587ab8e65a61f6f019c72966d551a5916c70
ViniciusHP <viniciushp97@gmail.com>
2024-01-18 15:31:13 -0300

    Aula Coerção automática

---
+++ cod3r/python-3/curso-python/notas/coercao-automatica.py
@@ -0,0 +1,21 @@
+# Coerção Automática
+
+print(10 / 2)
+print(type(10 / 2))
+print(10 / 3)
+
+# Valor inteiro
+print(10 // 3)
+print(type(10 // 3))
+
+# Valor float
+print(10 // 3.3)
+print(type(10 // 3.3))
+
+# True = 1
+print(2 + True)
+# False = 0
+print(2 + False)
+
+print(type(1 + 2))
+print(type(1 + 2.5))
