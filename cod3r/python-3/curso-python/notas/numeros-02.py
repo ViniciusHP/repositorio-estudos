ceaee6314aeb69a9855b03ba011d6f48e6459f25
ViniciusHP <viniciushp97@gmail.com>
2024-01-18 15:42:43 -0300

    Aula Números #02

---
+++ cod3r/python-3/curso-python/notas/numeros-02.py
@@ -0,0 +1,14 @@
+# Importando do módulo "decimal"
+from decimal import Decimal, getcontext
+
+print(Decimal(1) / Decimal(7))
+
+getcontext().prec = 4
+
+
+print(Decimal(1) / Decimal(7))
+
+print(Decimal.max(Decimal(1), Decimal(7)))
+
+print(1.1 + 2.2)
+print(Decimal(1.1) + Decimal(2.2))
