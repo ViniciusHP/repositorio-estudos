adcb3b55573018338367cc77240489668a45322f
ViniciusHP <viniciushp97@gmail.com>
2024-01-18 14:58:43 -0300

    Aula Operadores ternários

--- cod3r/python-3/curso-python/notas/desafio-operadores-logicos.py
+++ cod3r/python-3/curso-python/notas/desafio-operadores-logicos.py
@@ -41,3 +41,11 @@ not 1
 not -2
 not False
 not not True
+
+# Operadores Ternários
+
+esta_chovendo = False
+
+print('Hoje estou com as roupas ' + ('secas.', 'molhadas.')[esta_chovendo])
+print('Hoje estou com as roupas ' + ('molhadas.'
+                                     if esta_chovendo else 'secas.'))
