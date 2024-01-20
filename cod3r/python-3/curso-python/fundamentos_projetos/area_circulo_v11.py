6c40c7428af8dd475e27dc07f37210cfc5342efe
ViniciusHP <viniciushp97@gmail.com>
2024-01-20 14:44:56 -0300

    Aula melhorando o help

---
+++ cod3r/python-3/curso-python/fundamentos_projetos/area_circulo_v11.py
@@ -0,0 +1,17 @@
+#!python
+from math import pi
+import sys
+
+
+def area(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('É necessário informar o raio do círculo.')
+        print('Sintaxe: {} <raio>'.format(sys.argv[0][2:]))
+    else:
+        raio = sys.argv[1]
+        area_cirulo = area(raio)
+        print('Área do círculo', area_cirulo)
