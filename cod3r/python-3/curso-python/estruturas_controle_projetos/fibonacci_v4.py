43f5b82a529603c6e792143a13019f93f89afb92
ViniciusHP <viniciushp97@gmail.com>
2024-01-22 22:54:45 -0300

    Aula substituindo variáveis por lista

---
+++ cod3r/python-3/curso-python/estruturas_controle_projetos/fibonacci_v4.py
@@ -0,0 +1,11 @@
+#!python
+def fibonnacci(limite):
+    resultado = [0, 1]
+    while resultado[-1] < limite:
+        resultado.append(resultado[-2] + resultado[-1])
+    return resultado
+
+
+if __name__ == '__main__':
+    for fib in fibonnacci(10000):
+        print(fib)
