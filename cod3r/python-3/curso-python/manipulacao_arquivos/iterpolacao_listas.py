259ef1ea64a31870c0354fed3e153aabf3960161
ViniciusHP <viniciushp97@gmail.com>
2024-02-03 17:48:27 -0300

    Aula Leitura básica

---
+++ cod3r/python-3/curso-python/manipulacao_arquivos/iterpolacao_listas.py
@@ -0,0 +1,8 @@
+valoresTupla = ('Vinicius', 26)
+valoresLista = ('VINICIUS', 27)
+
+# Vai dar erro
+# print('Meu nome é {} e tenho {} anos.'.format(valoresTupla))
+# print('Meu nome é {} e tenho {} anos.'.format(valoresLista))
+print('Meu nome é {} e tenho {} anos.'.format(*valoresTupla))
+print('Meu nome é {} e tenho {} anos.'.format(*valoresLista))
