93c895bacec3d5241183792459e1ab940bc5b11a
ViniciusHP <viniciushp97@gmail.com>
2024-02-25 21:57:41 -0300

    Aula Packing & Unpacking Nomeado #02

--- cod3r/python-3/curso-python/funcoes/gerador_html_v4.py
+++ cod3r/python-3/curso-python/funcoes/gerador_html_v4.py
@@ -1,7 +1,7 @@
 #!python
 def tag_bloco(conteudo, *args, classe='success', inline=False):
     tag = 'span' if inline else 'div'
-    html = conteudo if not callable(conteudo) else conteudo()
+    html = conteudo if not callable(conteudo) else conteudo(*args)
     return f'<{tag} class="{classe}">{html}</{tag}>'
 
 
