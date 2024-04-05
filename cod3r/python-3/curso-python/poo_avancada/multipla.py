066e0cbe5c423d26ecc4b58b205c11f2721f7bb2
ViniciusHP <viniciushp97@gmail.com>
2024-04-05 13:34:10 -0300

    Aula Herança Múltiplas

---
+++ cod3r/python-3/curso-python/poo_avancada/multipla.py
@@ -0,0 +1,35 @@
+#!python
+class Animal:
+    @property
+    def capacidades(self):
+        return ('dormir', 'comer', 'beber')
+
+
+class Homem(Animal):
+    @property
+    def capacidades(self):
+        return super().capacidades + ('amar', 'falar', 'estudar')
+
+
+class Aranha(Animal):
+    @property
+    def capacidades(self):
+        return super().capacidades + ('fazer teia', 'andar pelas paredes')
+
+
+class HomemAranha(Homem, Aranha):
+    @property
+    def capacidades(self):
+        return super().capacidades + \
+            ('bater em bandidos', 'atirar teias entre prédios')
+
+
+if __name__ == '__main__':
+    john = Homem()
+    print(f'John: {john.capacidades}')
+
+    aranha = Aranha()
+    print(f'Aranha: {aranha.capacidades}')
+
+    homem_aranha = HomemAranha()
+    print(f'HomemAranha: {homem_aranha.capacidades}')
