163d3c38b8c20c21ecede9647b7d1bc0183b62a8
ViniciusHP <viniciushp97@gmail.com>
2024-04-07 19:29:48 -0300

    Aula Consultar Registros

---
+++ cod3r/python-3/curso-python/banco-dados/src/selecionar_contatos.py
@@ -0,0 +1,19 @@
+from bd import nova_conexao
+from mysql.connector import ProgrammingError
+
+sql = 'SELECT * FROM contatos'
+
+try:
+    with nova_conexao() as conexao:
+        try:
+            cursor = conexao.cursor()
+            cursor.execute(sql)
+            contatos = cursor.fetchall()
+        except ProgrammingError as e:
+            print(f'Erro: {e.msg}')
+        else:
+            for contato in contatos:
+                print(f'{contato[2]:2d} - {contato[0]:10s} Telefone: {
+                    contato[1]}')
+except ProgrammingError as e:
+    print(f'Erro CONEXÃO: {e.msg}')
